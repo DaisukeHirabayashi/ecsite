aeffd7d5a209208dca42cf53aaefa58e7fa6754e
平林大典 <daisuke@pluslab.org>
2020-01-21 14:19:42 +0900

    簡易的な商品ページの作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <template v-for="{ id, name } in items">{{ id }}{{ name }}</template>
+    <v-container flud>
+      <Products :items="items" />
+    </v-container>
   </div>
 </template>
 <script>
+import Products from "../components/Products.vue";
 export default {
+  components: {
+    Products
+  },
   name: "home",
   data() {
     return {
       items: [
-        { id: 1, name: "aaa" },
-        { id: 2, name: "bbb" },
-        { id: 3, name: "ccc" }
+        { id: 1, name: "aaa", show: false },
+        { id: 2, name: "bbb", show: false },
+        { id: 3, name: "ccc", show: false },
+        { id: 4, name: "ddd", show: false }
       ]
     };
   }
