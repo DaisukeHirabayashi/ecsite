c9bbfe90bf47c21da215bbd4c3ce4f9d87c59fde
平林大典 <daisuke@pluslab.org>
2020-01-31 13:48:04 +0900

    登録処理

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -7,6 +7,7 @@
       <RegisterForm :formData="formData[1]" />
       <RegisterForm :formData="formData[2]" />
       <RegisterForm :formData="formData[3]" />
+      <RegisterForm :formData="formData[4]" />
       <v-btn type="button" name="button" @click="doSend">登録</v-btn>
     </v-container>
   </div>
@@ -19,6 +20,7 @@ export default {
   },
   data() {
     return {
+      info: "",
       formData: [
         {
           displayname: "メールアドレス",
@@ -39,6 +41,12 @@ export default {
           rules: [v => !!v || "user name is required"]
         },
         {
+          displayname: "住所",
+          formText: "",
+          placeholder: "address",
+          rules: [v => !!v || "address is required"]
+        },
+        {
           displayname: "電話番号",
           formText: "",
           placeholder: "phone number",
@@ -53,15 +61,39 @@ export default {
         this.formData[0].formText.length &&
         this.formData[1].formText.length &&
         this.formData[2].formText.length &&
-        this.formData[3].formText.length
+        this.formData[3].formText.length &&
+        this.formData[4].formText.length
       ) {
-        document.location.href = "./";
+        //axiosの通信;
+        this.error = null;
+        const axios = require("axios");
+        (async () => {
+          await axios({
+            method: "POST",
+            url: "http://104.198.57.17:5000/register",
+            data: {
+              mailaddress: this.formData[0].formText,
+              password: this.formData[1].formText,
+              username: this.formData[2].formText,
+              address: this.formData[3].formText,
+              phonenumber: this.formData[4].formText
+            }
+          })
+            .then(response => (this.info = response.data))
+            .catch(error => console.log(error.response));
+          if (this.info == "success") {
+            alert("登録が完了しました");
+            document.location.href = "./";
+          }
+        })();
       } else if (!this.formData[0].formText.length) {
-        alert("会社名を入力してください");
-      } else if (!this.formData[1].formText.length) {
         alert("住所を入力してください");
+      } else if (!this.formData[1].formText.length) {
+        alert("パスワードを入力してください");
       } else if (!this.formData[2].formText.length) {
-        alert("URLを入力してください");
+        alert("ユーザーネームを入力してください");
+      } else if (!this.formData[3].formText.length) {
+        alert("住所を入力してください");
       } else {
         alert("電話番号を入力してください");
       }
