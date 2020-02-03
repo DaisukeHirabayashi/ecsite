7110f0fdc0b9b3feb9bf8f24927d00dbc2e2adf4
平林大典 <daisuke@pluslab.org>
2020-02-03 23:08:05 +0900

    レビュー情報の表示

---
+++ src/components/Review.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="small">
+    <v-row>
+      <v-col sm="30">
+        <v-card class="ma-1">
+          <radar-chart :chart-data="datacollection"></radar-chart>
+        </v-card>
+      </v-col>
+      <v-col sm="30">
+        <v-card class="ma-1">
+          <v-subheader>使いやすさ</v-subheader>
+          <v-card-text class="pt-0">
+            <v-slider
+              v-model="usability"
+              step="1"
+              :max="5"
+              :min="1"
+              thumb-label="always"
+              ticks
+            ></v-slider>
+          </v-card-text>
+          <v-subheader>聴き心地</v-subheader>
+          <v-card-text class="pt-0">
+            <v-slider
+              v-model="sound"
+              step="1"
+              :max="5"
+              :min="1"
+              thumb-label="always"
+              ticks
+            ></v-slider>
+          </v-card-text>
+          <v-subheader>見た目</v-subheader>
+          <v-card-text class="pt-0">
+            <v-slider
+              v-model="visual"
+              step="1"
+              :max="5"
+              :min="1"
+              thumb-label="always"
+              ticks
+            ></v-slider>
+          </v-card-text>
+          <v-subheader>つけ心地</v-subheader>
+          <v-card-text class="pt-0">
+            <v-slider
+              v-model="comfortable"
+              step="1"
+              :max="5"
+              :min="1"
+              thumb-label="always"
+              ticks
+            ></v-slider>
+          </v-card-text>
+          <v-subheader>遮音性</v-subheader>
+          <v-card-text class="pt-0">
+            <v-slider
+              v-model="blocking"
+              step="1"
+              :max="5"
+              :min="1"
+              thumb-label="always"
+              ticks
+            ></v-slider>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import RadarChart from "../plugins/chart.js";
+
+export default {
+  name: "Review",
+  props: {
+    datacollection: Object
+  },
+  components: {
+    RadarChart
+  },
+  data() {
+    return {
+      usability: 3,
+      sound: 3,
+      visual: 3,
+      comfortable: 3,
+      blocking: 3
+    };
+  }
+};
+</script>
+
+<style>
+.small {
+  max-width: 450px;
+}
+</style>
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,19 +1,38 @@
 <template>
+  <!-- 商品の詳細を出しているページのvue -->
   <div class="product">
-    <ProductDetail :product="product" :itemsrc="itemsrc" />
+    <v-container class="grey lighten-5">
+      <ProductDetail :product="product" :itemsrc="itemsrc" />
+      <Review :datacollection="datacollection" />
+    </v-container>
   </div>
 </template>
 <script>
 import ProductDetail from "../components/ProductDetail.vue";
+import Review from "../components/Review.vue";
 export default {
   name: "product",
   components: {
-    ProductDetail
+    ProductDetail,
+    Review
   },
   data() {
     return {
       product: this.$store.state.itemDetail,
-      itemsrc: this.$store.state.itemDetailSrc
+      itemsrc: this.$store.state.itemDetailSrc,
+      datacollection: {
+        labels: ["聴き心地", "遮音性", "見た目", "付け心地", "使いやすさ"],
+        datasets: [
+          {
+            label: "Aさん",
+            data: [100, 72, 86, 74, 86],
+            backgroundColor: "RGBA(225,95,150, 0.5)",
+            borderColor: "RGBA(225,95,150, 1)",
+            borderWidth: 1,
+            pointBackgroundColor: "RGB(46,106,177)"
+          }
+        ]
+      }
     };
   }
 };
